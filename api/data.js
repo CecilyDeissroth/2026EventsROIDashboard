1c478b02cec079c3eea2039211349ceea356231d
CecilyDeissroth <cecilylawson95@gmail.com>
2026-03-20 16:14:48 -0400

    Update data.js

--- api/data.js
+++ api/data.js
@@ -10,7 +10,7 @@
 //   4. Paste the bin ID into JSONBIN_BIN below and redeploy
 
 const JSONBIN_KEY  = '$2a$10$ka/Yb5bMEGPaOPNtkmqtgOfdXhFzbtmQKJ00yn4djYWrsl3fpblwO';
-const JSONBIN_BIN  = '';   // paste bin ID here after running /api/data/init
+const JSONBIN_BIN  = '69bd5b96c3097a1dd542a29c';   // paste bin ID here after running /api/data/init
 const API_SECRET   = '2026events';
 const JSONBIN_BASE = 'https://api.jsonbin.io/v3';
 
